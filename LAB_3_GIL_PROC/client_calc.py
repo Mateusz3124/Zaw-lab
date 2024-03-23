fa4107639d81929fbf9b990362acc9f51f7cc76b
ThinCan <01169581@pw.edu.pl>
2024-03-23 17:44:38 +0100

    added pool

--- LAB_3_GIL_PROC/client_calc.py
+++ LAB_3_GIL_PROC/client_calc.py
@@ -5,11 +5,10 @@ class QueueManager(BaseManager):
     pass
 
 def f(matrix_index, matrix_row, vec, j):
-    QueueManager.register('out_queue')
+    sum = 0.0
     m = QueueManager(address=('127.0.0.1', 8888), authkey=bytes('abracadabra', encoding='utf-8'))
     m.connect()
     out_queue = m.out_queue()
-    sum = 0.0
 
     for i in range(len(matrix_row)):
         sum += matrix_row[i] * vec[0][i]
@@ -18,19 +17,20 @@ def f(matrix_index, matrix_row, vec, j):
 
 def main():
     QueueManager.register('in_queue')
+    QueueManager.register('out_queue')
     m = QueueManager(address=('127.0.0.1', 8888), authkey=bytes('abracadabra', encoding='utf-8'))
     m.connect()
     in_queue = m.in_queue()
 
-    while True:
-        data = in_queue.get()
-        with Pool(processes=10) as pool:
+    with Pool(processes=10) as pool:
+        while True:
+            data = in_queue.get()
 
             for i in range(len(data[1])):
                 pool.apply_async(f, (data[0], data[1][i], data[2], i))
             
-            pool.close()
-            pool.join()
+        pool.close()
+        pool.join()
 
 if __name__ == '__main__':
 	main()
--- LAB_3_GIL_PROC/timing_tests.py
+++ LAB_3_GIL_PROC/timing_tests.py
@@ -13,7 +13,7 @@ def main(ip, port):
 	if(len(vec[0]) == 1):
 		vec = from_row_to_col_major_vec(vec)
 
-	for i in range(2, len(vec[0]), max(1, int(len(vec[0])/10))):
+	for i in range(50, len(vec[0]), max(1, int(len(vec[0])/10))):
 		num_procs = i	
 
 		if len(vec[0]) != len(mat[0]):
@@ -26,6 +26,7 @@ def main(ip, port):
 		read_and_get_answers(ip, port, num_procs, mat, vec)
 		t2 = time.time()
 		print(f"Num procs: {i}, time: {t2-t1} [s]")
+		break
 
 if __name__ == '__main__':
 	main('127.0.0.1', 8888)
