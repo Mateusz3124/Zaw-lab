c084d6e76a0c750962779a64d686c7e80b3244e1
ThinCan <01169581@pw.edu.pl>
2024-03-23 15:58:59 +0100

    added timing test

--- LAB_3_GIL_PROC/timing_tests.py
+++ LAB_3_GIL_PROC/timing_tests.py
@@ -1,5 +1,6 @@
 from client_read import read_and_get_answers, read
 import time
+import math
 
 def from_row_to_col_major_vec(row_vec):
 	col_vec = [[x[0] for x in row_vec]]
@@ -9,9 +10,10 @@ def from_row_to_col_major_vec(row_vec):
 def main(ip, port):
 	mat = read("./BIG_A.dat")
 	vec = read("./BIG_X.dat")
-	vec = from_row_to_col_major_vec(vec)
+	if(len(vec[0]) == 1):
+		vec = from_row_to_col_major_vec(vec)
 
-	for i in range(2, len(mat)-1, int(len(vec[0])/10)):
+	for i in range(2, len(vec[0]), max(1, int(len(vec[0])/10))):
 		num_procs = i	
 
 		if len(vec[0]) != len(mat[0]):
@@ -23,7 +25,7 @@ def main(ip, port):
 		t1 = time.time()
 		read_and_get_answers(ip, port, num_procs, mat, vec)
 		t2 = time.time()
-		print(t2 - t1)
+		print(f"Num procs: {i}, time: {t2-t1} [s]")
 
 if __name__ == '__main__':
 	main('127.0.0.1', 8888)
